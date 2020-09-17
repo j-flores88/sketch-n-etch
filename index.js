222091bf3edfd5018a34d64ba549a679acb24e1c
Joel Flores <j_flores88@outlook.com>
2020-09-16 18:09:28 -0700

    Add basic html and created sketch pad with js and css

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ if ($) {
     alert('SUCCESS!')
 } 
 */
+const grid = document.getElementById('container')
+
+
+function createGrid() {
+
+    for(let i = 0; i < 256; i++) {
+        const cell = document.createElement('div');
+        grid.appendChild(cell).className = 'square'
+    }
+}
+
+createGrid()
